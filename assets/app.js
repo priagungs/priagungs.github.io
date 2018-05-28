c3f31be88af004377f80eae291892186eb19b030
priagungs <32959030+priagungs@users.noreply.github.com>
2018-05-28 16:45:36 +0700

    article added

---
+++ assets/app.js
@@ -0,0 +1,17 @@
+// var headerNavs = document.getElementsByClassName('btn');
+// console.log(headerNavs.length);
+// for(var idx = 0; idx < headerNavs.length; i++){
+//     console.log('in');
+//     headerNavs[idx].addEventListener('onmouseover', function(){
+//         // console.log('in');
+//         var num = 0;
+//         var animate = setInterval(function(){
+//             if(headerNavs[idx].style.opacity == 0.8){
+//                 clearInterval(animate);
+//             }
+//             else{
+//                 headerNavs[idx].style.opacity += 0.05;
+//             }
+//         }, 5)
+//     })
+// }
